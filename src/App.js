1d5dd58425fdb87331b271b22fa884102df9a39e
tristan99 <ariescacy9@gmail.com>
2022-08-30 11:54:42 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [gastos, setGastos] = useState([0])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='App__gastos'>
+        <Contador gastos={gastos}/>
+        <GastosMes gastos={gastos}/>
+        <AgregarGasto setGastos = {setGastos} />
+      </div>
+      
+    </>
   );
+}
+
+function Contador ({gastos}){
+  return <div> Gasto mensual: {Suma({gastos})}</div>;
+}
+
+function Suma({gastos}){
+  let sum = 0;
+  for (let i = 0; i < gastos.length; i++) {
+    sum += Number(gastos[i]);
+  }
+  return(sum);
+}
+
+function GastosMes ({gastos}){
+
+return (
+  <ul>
+    {gastos.map ((gasto) =>( <li key = {gasto}>{gasto}</li>
+    ))}
+  </ul>
+);
+}
+
+function AgregarGasto ({setGastos}) {
+  function handleSubmit (event) {
+    event.preventDefault ();
+    const gasto = event.target.elements.gasto.value;
+    setGastos(e => [...e, gasto])
+  }
+
+return (
+  <form onSubmit = {handleSubmit}>
+    <input type = "number" id = "gasto"/>
+    <button type="Submit"> Agregar gasto </button>
+  </form>
+)
 }
 
 export default App;
